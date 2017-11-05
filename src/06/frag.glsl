2a37d43e48bc864b138dd767484e665e6d46d9de
Jordan Santell <jordan@jsantell.com>
2017-11-05 13:49:07 -0800

    glitch 06

--- src/06/frag.glsl
+++ src/06/frag.glsl
@@ -21,24 +21,24 @@ const float PI = 3.141592653589793;
 const float LOOP = 1000.0;
 const float LENGTH = 0.7;
 const float SPEED = 3.0;
-const float MOD = 1.0;
+const float MOD = 2.0;
 const float WAVE_SPEED = 0.001;
-const float WAVE_DEPTH = 0.03;
-const float COLUMNS = 30.0;
-const float ROWS = 30.0;
+const float WAVE_DEPTH = 0.05;
+const float COLUMNS = 20.0;
+const float ROWS = 20.0;
 const float F = 6.0;
 
 void main() {
   vec2 st = gl_FragCoord.xy / uResolution.xy;
   vec2 pos = st / -2.0 + 1.0;
-  float x = gl_FragCoord.x; //floor(pos.x * COLUMNS);
-  float y = pos.y; // floor(pos.y * ROWS) / ROWS;
+  float x = floor(pos.x * COLUMNS);
+  float y = floor(pos.y * ROWS) / ROWS;
   float n = noise(x * MOD);
   float speed = clamp(n, 0.1, 20.0);
 
   float elapsed = easeIn(mod((uTime * SPEED*speed*0.5) + (LOOP * n), LOOP) * 0.001) * 1.0;
   vec3 color = vec3(0.0);
-
+  
   float length = map(sin(0.5 * pos.x*F+(uTime * WAVE_SPEED) + x * PI * 2.0 - PI), 0.0, 1.0, LENGTH, LENGTH + WAVE_DEPTH);
 
   // lines
@@ -46,9 +46,19 @@ void main() {
   float belowLine = when_lt(elapsed, y);
   float smooth = smoothstep(elapsed - y, elapsed, y);
   float isLine = aboveLine * belowLine;
-
+ 
   // color
-  color = when_lt(0.0, isLine) * vec3(1.0);
+  color = when_lt(0.0, isLine) * vec3(0.6, 1.0 - pos.y, 0.5);
+  color.r *= when_lt(length, pos.y) * 1.0;
+ 
+  float diff = 0.1;
+  float length2 = map(sin(2.0+0.8*pos.x*F+(uTime * WAVE_SPEED) + x * PI * 2.0 - PI), 0.0, 1.0, LENGTH - diff, LENGTH + WAVE_DEPTH - diff);
+  color -= when_lt(y, elapsed+length2) * when_lt(elapsed,y) * vec3(0.1);
+  
+  diff = 0.15;
+  length2 = map(sin(0.7*pos.x*F+(uTime * WAVE_SPEED) + x * PI * 2.0 - PI), 0.0, 1.0, LENGTH - diff, LENGTH + WAVE_DEPTH - diff);
+  color -= when_lt(y, elapsed+length2) * when_lt(elapsed,y) * vec3(0.1);
 
+  color *= vec3(easeOut(pos.y)) * 2.0;
   gl_FragColor = vec4(color, 1.0);
 }
